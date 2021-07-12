d449182fabe0bcde180eedf7857b9aa2f31ed077
FPF <49359900+Curtin0@users.noreply.github.com>
2021-07-12 23:02:46 +0800

    Add files via upload

--- COMtoSocket.py
+++ COMtoSocket.py
@@ -1,40 +1,38 @@
 import serial
 from time import sleep
 from socket import *
-import binascii  
-import struct
 
-HOST = 'localhost'
+HOST = '112.74.182.249'
 PORT = 20019
-BUFSIZ =1024
-ADDR = (HOST,PORT)
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
 
-tcpCliSock = socket(AF_INET,SOCK_STREAM)
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
 tcpCliSock.connect(ADDR)
+
 
 def recv(serial):
     while True:
-        data = serial.read_all().decode("gbk","ignore")
+        data = serial.read_all().decode("utf-8","ignore")
         if data == '':
             continue
         else:
             break
-        sleep(0.2)
+    sleep(2)
     return data
 
 if __name__ == '__main__':
-    serial = serial.Serial('COM3', 19200, timeout=1)
+    serial = serial.Serial('COM2', 19200, timeout=2)
     if serial.isOpen():
         print("serial open success")
     else:
         print("serial open failed")
     while True:
         data = recv(serial)      
-        tcpCliSock.send(data.encode('utf-8',"ignore"))
+        tcpCliSock.send(data.encode('utf-8'))
         data = tcpCliSock.recv(BUFSIZ)
         if not data:
-            berak       
-        print(data.decode('utf-8',"ignore"))
+            break       
+        print(data.decode('utf-8'))
         
     tcpCliSock.close()
-        
