d3043e39fb1cc7ebd643f5fc0dde9e40d9366231
FPF <49359900+Curtin0@users.noreply.github.com>
2021-07-13 14:50:25 +0800

    Add files via upload

--- COMtoSocket.py
+++ COMtoSocket.py
@@ -1,8 +1,11 @@
+
 import serial
 from time import sleep
 from socket import *
 
+
 HOST = '112.74.182.249'
+#HOST = '127.0.0.1'
 PORT = 20019
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
@@ -12,13 +15,17 @@ tcpCliSock.connect(ADDR)
 
 
 def recv(serial):
+    print("enter receive")
     while True:
-        data = serial.read_all().decode("utf-8","ignore")
+        '读取串口数据'  
+        data = serial.read_all().decode("gbk","ignore")
+        
         if data == '':
+            
             continue
         else:
             break
-    sleep(2)
+        
     return data
 
 if __name__ == '__main__':
@@ -28,11 +35,27 @@ if __name__ == '__main__':
     else:
         print("serial open failed")
     while True:
-        data = recv(serial)      
-        tcpCliSock.send(data.encode('utf-8'))
-        data = tcpCliSock.recv(BUFSIZ)
-        if not data:
-            break       
-        print(data.decode('utf-8'))
+        '运行函数 读取数据'
+        data = recv(serial)
         
+        '检查收到的串口数据并打印出来'        
+        by = bytes(data,'gbk')
+        print(by.hex())
+        
+        '向服务器发数据'
+        tcpCliSock.send(data.encode("gbk","ignore"))
+
+        '接收来自服务器的回复'
+        dataRec = tcpCliSock.recv(BUFSIZ)
+        if not data:
+            break
+        
+        '用hex格式打印已经接收到的服务端回复'
+        serialdata = bytes(dataRec.decode('utf-8',"ignore"),'utf-8').hex()
+        print(serialdata)
+        
+        '将服务端回复发给串口'
+        result =serial.write(dataRec)
+        print("写入",result)
+              
     tcpCliSock.close()
