491977f5e1e03d9238de2af7975868ef3533031d
FPF <49359900+Curtin0@users.noreply.github.com>
2021-07-16 21:21:38 +0800

    Add files via upload

--- COMtoSocket.py
+++ COMtoSocket.py
@@ -2,7 +2,6 @@ import serial
 from time import sleep
 from socket import *
 
-
 HOST = '112.74.182.249'
 PORT = 20019
 BUFSIZ = 1024
@@ -10,7 +9,6 @@ ADDR = (HOST, PORT)
 
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 tcpCliSock.connect(ADDR)
-
 
 def recv(serial):
     print("enter receive")
@@ -20,8 +18,7 @@ def recv(serial):
         if data == '':            
             continue
         else:
-            break
-        
+            break       
     return data
 
 if __name__ == '__main__':
@@ -32,11 +29,7 @@ if __name__ == '__main__':
         print("serial open failed")
     while True:
         '运行函数 读取数据'
-        data = recv(serial)
-        
-        '检查收到的串口数据并打印出来'        
-        #by = bytes(data)
-        #print(by.hex())
+        data = recv(serial)       
         
         '向服务器发数据'
         tcpCliSock.send(bytes.fromhex(data))
@@ -45,10 +38,6 @@ if __name__ == '__main__':
         dataRec = tcpCliSock.recv(BUFSIZ)
         if not data:
             break
-        
-        '用hex格式打印已经接收到的服务端回复'
-        serialdata = bytes(dataRec.decode('utf-8',"ignore"),'utf-8').hex()
-        print(serialdata)
         
         '将服务端回复发给串口'
         result =serial.write(dataRec)
