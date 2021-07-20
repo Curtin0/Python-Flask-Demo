90090b93caa575dbae0dfcf53116ff84c881b408
FPF <49359900+Curtin0@users.noreply.github.com>
2021-07-21 00:05:34 +0800

    Add files via upload
    添加了crc校验

--- CloudServer.py
+++ CloudServer.py
@@ -9,6 +9,22 @@ server = socket.socket()
 server.bind(('', 20019))
 server.listen()
 
+#CRC校验算法
+
+
+def calc_crc(bytedata):
+    data = bytearray(bytedata)
+    crc = 0xFFFF
+    for pos in data:
+        crc ^= pos
+        for i in range(8):
+            if ((crc & 1) != 0):
+                crc >>= 1
+                crc ^= 0xA001
+            else:
+                crc >>= 1
+    return hex(((crc & 0xff) << 8) + (crc >> 8))
+
 # socke服务端接收数据
 # 收到的数据格式 21 41 01 00 1A 00 00 00 02 00 00 00 00 03 02 03 E8 00 28 00 6E 0B B8 0B B8 0B B8 00 00 4E 20 00 01 02 03 86 44
 
@@ -17,6 +33,9 @@ def recv(conn):
     while True:
         # 如果检测到丢包则重新接收数据
         data = conn.recv(1024)
+        # data[:35]
+        # data[35:37]
+
         try:
             dataList = list(data)
             # 检测校验码0x8644
@@ -91,37 +110,44 @@ def Webdata(dataList):
 
 # 主函数 循环等待socket客户端发来消息
 if __name__ == '__main__':
+  while True:
+    conn, addr = server.accept()
     while True:
-        conn, addr = server.accept()
-        while True:
-            async def echo(websocket, path):
-                while True:
-                    data = recv(conn)
-                    time.sleep(3)
-                    dataList = list(data)
-                    print("数据转换DEC存入数组后为")
-                    print(dataList)
+        async def echo(websocket, path):
+            while True:
+                data = recv(conn)
+                dataList = list(data)
+                print("数据转换DEC存入数组后为")
+                print(dataList)
 
-                    # 转码json发送数据给前端
-                    MessageJson = json.dumps(Webdata(dataList))
-                    await websocket.send(MessageJson)
-                    await asyncio.sleep(3)
+                # 转码json发送数据给前端
+                MessageJson = json.dumps(Webdata(dataList))
+                await websocket.send(MessageJson)
+                await asyncio.sleep(3)
 
-                    # 从前端接收json数据
-                    MessageReceive = await websocket.recv()
-                    MessageReceiveJson = json.loads(MessageReceive)
+                # 从前端接收json数据
+                MessageReceive = await websocket.recv()
+                MessageReceiveJson = json.loads(MessageReceive)
 
-                    # 打印json解码后的数组
-                    print("收到前端数据")
-                    print(MessageReceiveJson)
+                # 打印json解码后的数组
+                print("收到前端数据")
+                print(MessageReceiveJson)
 
-                    # dec数据转换hex
-                    bytedata = bytes(MessageReceiveJson)
-                    print(bytedata)
+                # dec数据转换hex  before表示未添加CRC校验码的值
+                bytedatabefore = bytes(MessageReceiveJson)
+                #计算CRC校验值并加入到MessageReceiveJson数组中
+                crcstr = calc_crc(bytedatabefore)
+                t = crcstr.encode("utf-8")
+                x = int(t[2:4], 16)
+                y = int(t[4:6], 16)
+                MessageReceiveJson.append(x)
+                MessageReceiveJson.append(y)
 
-                    # 发送hex格式给Socket客户端
-                    conn.send(bytedata)
+                bytedata = bytes(MessageReceiveJson)
 
-            start_server = websockets.serve(echo, '', 20020)
-            asyncio.get_event_loop().run_until_complete(start_server)
-            asyncio.get_event_loop().run_forever()
+                # 发送hex格式给Socket客户端
+                conn.send(bytedata)
+
+        start_server = websockets.serve(echo, '', 20020)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
